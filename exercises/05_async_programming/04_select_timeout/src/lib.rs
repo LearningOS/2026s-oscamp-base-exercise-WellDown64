79fc3e4b7046c643d5e370b05e7c2a75441bbf5d
welldown <hangw7252@gmail.com>
2026-03-21 15:13:10 +0800

    complete ex 5-4

--- exercises/05_async_programming/04_select_timeout/src/lib.rs
+++ exercises/05_async_programming/04_select_timeout/src/lib.rs
@@ -21,7 +21,11 @@ where
 {
     // TODO: Use tokio::select! to race between future and sleep
     // Or use tokio::time::timeout
-    todo!()
+    
+    tokio::select! {
+        output = future => Some(output),
+        _ = sleep(Duration::from_millis(timeout_ms)) => None,
+    }
 }
 
 /// Race two async tasks, return the result of whichever finishes first.
@@ -34,7 +38,10 @@ where
 {
     // TODO: Use tokio::select! to wait for f1 and f2
     // Return the result of whichever completes first
-    todo!()
+    tokio::select! {
+        output1 = f1 => output1,
+        output2 = f2 => output2,
+    }
 }
 
 #[cfg(test)]
